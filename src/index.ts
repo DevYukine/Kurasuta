97c94a74a4c5fea628a054ee58ec3dc022f4dc81
Yukine <blazylp@gmail.com>
2018-11-11 12:28:16 +0100

    [Feature] Export ShardClientUtil from index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { BaseCluster } from './Cluster/BaseCluster';
 import { ShardingManager, version } from './Sharding/ShardingManager';
 import { Util } from './Util/Util';
+import { ShardClientUtil } from './Sharding/ShardClientUtil';
 
-export { BaseCluster, ShardingManager, Util, version };
+export { BaseCluster, ShardingManager, Util, ShardClientUtil, version };
