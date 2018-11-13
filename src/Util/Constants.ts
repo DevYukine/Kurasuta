4d68eff66a39631db1cd13e95517dca34be149c1
Yukine <blazylp@gmail.com>
2018-11-13 15:53:27 +0100

    [Fix] Fix using package.json for version export

--- src/Util/Constants.ts
+++ src/Util/Constants.ts
@@ -3,6 +3,8 @@ export const http = {
 	api: 'https://discordapp.com/api'
 };
 
+export const version = '0.2.13';
+
 export enum IPCEvents {
 	EVAL,
 	MESSAGE,
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { ShardingManager } from './Sharding/ShardingManager';
 import { ShardClientUtil } from './Sharding/ShardClientUtil';
 import { BaseCluster } from './Cluster/BaseCluster';
-import { version } from '../package.json';
+import { version } from './Util/Constants';
 import * as Util from './Util/Util';
 
 export { BaseCluster, ShardingManager, Util, ShardClientUtil, version };
