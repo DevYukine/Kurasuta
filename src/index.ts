9c8be0fce57680366f58ce16ffce14f513c81ddd
Yukine <blazylp@gmail.com>
2019-09-07 15:16:51 +0200

    [Feat] Add missing methods & overloads to Djs ShardClientUtil
    fixes #146

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,28 @@ import { Cluster, ClusterOptions } from './Cluster/Cluster';
 import { ClusterIPC } from './IPC/ClusterIPC';
 import { MasterIPC } from './IPC/MasterIPC';
 import * as Util from './Util/Util';
+import { SendOptions } from 'veza';
+
+declare module 'discord.js' {
+	interface ShardClientUtil {
+		ipcSocket: string | number;
+		readonly clusterCount: number;
+		readonly shardCount: number;
+		readonly id: number;
+		readonly ipc: ClusterIPC;
+		broadcastEval(script: string | Function): Promise<unknown[]>;
+		masterEval(script: string | Function): Promise<unknown>;
+		fetchClientValues(prop: string): Promise<unknown[]>;
+		fetchGuild(id: string): Promise<object>;
+		fetchUser(id: string): Promise<object>;
+		fetchChannel(id: string): Promise<object>;
+		restartAll(): Promise<void>;
+		restart(clusterID: number): Promise<void>;
+		respawnAll(): Promise<void>;
+		send(data: any, options?: SendOptions): Promise<unknown>;
+		init(): Promise<void>;
+	}
+}
 
 export {
 	BaseCluster,
