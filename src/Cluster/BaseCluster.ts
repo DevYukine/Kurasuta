2520bf06e71f665b2ed649e34ea531a2a48fc30d
Yukine <devyukine@gmx.de>
2020-08-31 19:24:41 +0200

    [Deps] Update eslint-config-marine to v7.2.0

--- src/Cluster/BaseCluster.ts
+++ src/Cluster/BaseCluster.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { ShardingManager } from '..';
 import { Client, ClientOptions } from 'discord.js';
 import { ShardClientUtil } from '../Sharding/ShardClientUtil';
--- src/Util/Constants.ts
+++ src/Util/Constants.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 export const http = {
 	version: 7,
 	api: 'https://discordapp.com/api'
